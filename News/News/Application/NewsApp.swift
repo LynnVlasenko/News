8cedec891222c85c29174d3f01c4cbd9d49096fd
LynnVlasenko <lynn.vlasenko@gmail.com>
2024-06-19 14:29:08 +0200

    Added project setup
    - Сreated a repository,
    - Сreated a project,
    - Sorted files by folders,
    - Set project settings

---
+++ News/News/Application/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Алина Власенко on 19.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
